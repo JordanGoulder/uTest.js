4d0915eaf40b91fa2889b86de31bbdc9907da0f2
Jordan Goulder <jordan.goulder@gmail.com>
2014-02-14 19:12:05 -0500

    Add more assertions

--- utest.js
+++ utest.js
@@ -16,8 +16,65 @@ uTest = {
       this.testGroups[test.group].tests.push(test);
    },
 
-   FAIL: function (str) {
-      throw new Error(str);
+   CHECK: function (condition) {
+      var errorString = this.getErrorString() + "\tCHECK failed";
+
+      if (condition !== true) {
+         throw new Error(errorString);
+      }
+   },
+
+   CHECK_TEXT: function (condition, text) {
+      var errorString = this.getErrorString() + "\tMessage: " + text + "\n\tCHECK failed";
+      if (condition !== true) {
+         throw new Error(errorString);
+      }
+   },
+
+   CHECK_EQUAL: function (expected, actual) {
+      var errorString = this.getErrorString() + "\texpected <" + expected  + ">\n" +
+                                                "\tbut was  <" + actual    + ">";
+      if (expected !== actual) {
+         throw new Error(errorString);
+      }
+   },
+
+   STRCMP_EQUAL: function (expected, actual) {
+      var errorString = this.getErrorString() + "\texpected <" + expected.toString()   + ">\n" +
+                                                "\tbut was  <" + actual.toString()     + ">";
+      if (expected.toString() !== actual.toString()) {
+         throw new Error(errorString);
+      }
+   },
+
+   LONGS_EQUAL: function (expected, actual) {
+      var errorString = this.getErrorString() + "\texpected <" + Math.floor(expected)  + ">\n" +
+                                                "\tbut was  <" + Math.floor(actual)    + ">";
+      if (Math.floor(expected) !== Math.floor(actual)) {
+         throw new Error(errorString);
+      }
+   },
+
+   DOUBLES_EQUAL: function (expected, actual, tolerance) {
+      var errorString = this.getErrorString() + "\texpected <" + expected  + ">\n"  +
+                                                "\tbut was  <" + actual    + ">"    +
+                                                " threshold was <" + tolerance + ">";
+      if (Math.abs(expected - actual) > tolerance) {
+         throw new Error(errorString);
+      }
+   },
+
+   FAIL: function (text) {
+      var errorString = this.getErrorString() + "\t" + text;
+      throw new Error(errorString);
+   },
+
+   getErrorString: function () {
+      var errorString = "error: Failiure in TEST(" +
+                        this.currentGroup + ", "   +
+                        this.currentTest + ")\n";
+
+      return errorString;
    },
 
    runAllTests: function () {
@@ -31,9 +88,14 @@ uTest = {
 
    runTestGroup: function (group) {
       console.log("Running test group " + group.name);
+
+      this.currentGroup = group.name;
+
       try {
          for (var i = 0; i < group.tests.length; i++)
          {
+            this.currentTest = group.tests[i].name;
+
             if (typeof group.setup === "function") {
                group.setup();
             }
@@ -49,7 +111,7 @@ uTest = {
             console.log("Passed");
          }
       } catch (ex) {
-         console.log("Failed");
+         console.log(ex.message);
       }
    }
 }
