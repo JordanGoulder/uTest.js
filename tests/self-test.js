b69da1b4aada1f57ac3da40af379358ee12ce952
Jordan Goulder <jordan.goulder@gmail.com>
2014-02-16 15:27:57 -0500

    Add test for failing checks

--- tests/self-test.js
+++ tests/self-test.js
@@ -2,6 +2,7 @@ uTest.TEST_GROUP({ name: "SelfTests",
 
    setup: function (test) {
       test.myTest = uTest.clone();
+      test.myTest.disableLogging();
    },
 
    teardown: function (test) {
@@ -39,5 +40,67 @@ uTest.TEST({ group: "SelfTests", name: "PassingChecks",
       uTest.CHECK(this.myTest._failCount      === 0);
       uTest.CHECK(this.myTest._runCount       === 1);
       uTest.CHECK(this.myTest._checkCount     === 7);
+   },
+});
+
+uTest.TEST({ group: "SelfTests", name: "FailingChecks",
+   run: function (uTest) {
+
+      this.myTest.TEST_GROUP({ name: "FailingChecksGroup" });
+
+      this.myTest.TEST({ group: "FailingChecksGroup", name: "BYTES_EQUAL",
+         run: function (uTest) {
+            uTest.BYTES_EQUAL(0x8f, 0x90);
+         },
+      });
+
+      this.myTest.TEST({ group: "FailingChecksGroup", name: "CHECK",
+         run: function (uTest) {
+            uTest.CHECK(false);
+         },
+      });
+
+      this.myTest.TEST({ group: "FailingChecksGroup", name: "CHECK_EQUAL",
+         run: function (uTest) {
+            uTest.CHECK_EQUAL(true, false);
+         },
+      });
+
+      this.myTest.TEST({ group: "FailingChecksGroup", name: "CHECK_TEXT",
+         run: function (uTest) {
+            uTest.CHECK_TEXT(1 === 0, "1 should not equal 0");
+         },
+      });
+
+      this.myTest.TEST({ group: "FailingChecksGroup", name: "DOUBLES_EQUAL",
+         run: function (uTest) {
+            uTest.DOUBLES_EQUAL(2.1, 2.3, 0.10001);
+         },
+      });
+
+      this.myTest.TEST({ group: "FailingChecksGroup", name: "LONGS_EQUAL",
+         run: function (uTest) {
+            uTest.LONGS_EQUAL(5, 6);
+         },
+      });
+
+      this.myTest.TEST({ group: "FailingChecksGroup", name: "STRCMP_EQUAL",
+         run: function (uTest) {
+            uTest.STRCMP_EQUAL("one", "two");
+         },
+      });
+
+      this.myTest.TEST({ group: "FailingChecksGroup", name: "FAIL",
+         run: function (uTest) {
+            uTest.FAIL("Fail me!");
+         },
+      });
+
+      this.myTest.runAllTests();
+
+      uTest.CHECK(this.myTest._getTestCount() === 8);
+      uTest.CHECK(this.myTest._failCount      === 8);
+      uTest.CHECK(this.myTest._runCount       === 8);
+      uTest.CHECK(this.myTest._checkCount     === 8);
    },
 });
