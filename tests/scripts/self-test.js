0c80150ddffe45ff338b0d11737a2f20a7998312
Jordan Goulder <jordan.goulder@gmail.com>
2014-02-19 21:36:54 -0500

    Remove trailing commas

--- tests/scripts/self-test.js
+++ tests/scripts/self-test.js
@@ -16,7 +16,7 @@ define (["../../src/uTest"], function (uTest) {
       run: function () {
          this.uTest.CHECK(Object.getPrototypeOf(this.myTest) === uTest);
          this.uTest.CHECK(this.myTest._getTestCount() === 0);
-      },
+      }
    });
 
    uTest.TEST({ group: "SelfTests", name: "Logging",
@@ -37,7 +37,7 @@ define (["../../src/uTest"], function (uTest) {
          this.myTest._verbose = true;
          this.myTest.disableVerboseLogging();
          this.uTest.CHECK_EQUAL(this.myTest._verbose, false);
-      },
+      }
    });
 
    uTest.TEST({ group: "SelfTests", name: "PassingChecks",
@@ -54,7 +54,7 @@ define (["../../src/uTest"], function (uTest) {
                this.uTest.DOUBLES_EQUAL(2.1, 2.2, 0.100001);
                this.uTest.LONGS_EQUAL(2, 2);
                this.uTest.STRCMP_EQUAL("one", "one");
-            },
+            }
          });
 
          this.myTest.runAllTests();
@@ -63,7 +63,7 @@ define (["../../src/uTest"], function (uTest) {
          this.uTest.CHECK(this.myTest._failCount      === 0);
          this.uTest.CHECK(this.myTest._runCount       === 1);
          this.uTest.CHECK(this.myTest._checkCount     === 7);
-      },
+      }
    });
 
    uTest.TEST({ group: "SelfTests", name: "FailingChecks",
@@ -74,49 +74,49 @@ define (["../../src/uTest"], function (uTest) {
          this.myTest.TEST({ group: "FailingChecksGroup", name: "BYTES_EQUAL",
             run: function () {
                this.uTest.BYTES_EQUAL(0x8f, 0x90);
-            },
+            }
          });
 
          this.myTest.TEST({ group: "FailingChecksGroup", name: "CHECK",
             run: function () {
                this.uTest.CHECK(false);
-            },
+            }
          });
 
          this.myTest.TEST({ group: "FailingChecksGroup", name: "CHECK_EQUAL",
             run: function () {
                this.uTest.CHECK_EQUAL(true, false);
-            },
+            }
          });
 
          this.myTest.TEST({ group: "FailingChecksGroup", name: "CHECK_TEXT",
             run: function () {
                this.uTest.CHECK_TEXT(1 === 0, "1 should not equal 0");
-            },
+            }
          });
 
          this.myTest.TEST({ group: "FailingChecksGroup", name: "DOUBLES_EQUAL",
             run: function () {
                this.uTest.DOUBLES_EQUAL(2.1, 2.3, 0.10001);
-            },
+            }
          });
 
          this.myTest.TEST({ group: "FailingChecksGroup", name: "LONGS_EQUAL",
             run: function () {
                this.uTest.LONGS_EQUAL(5, 6);
-            },
+            }
          });
 
          this.myTest.TEST({ group: "FailingChecksGroup", name: "STRCMP_EQUAL",
             run: function () {
                this.uTest.STRCMP_EQUAL("one", "two");
-            },
+            }
          });
 
          this.myTest.TEST({ group: "FailingChecksGroup", name: "FAIL",
             run: function () {
                this.uTest.FAIL("Fail me!");
-            },
+            }
          });
 
          this.myTest.runAllTests();
@@ -125,7 +125,7 @@ define (["../../src/uTest"], function (uTest) {
          this.uTest.CHECK(this.myTest._failCount      === 8);
          this.uTest.CHECK(this.myTest._runCount       === 8);
          this.uTest.CHECK(this.myTest._checkCount     === 8);
-      },
+      }
    });
 
    uTest.TEST({ group: "SelfTests", name: "LONGS_EQUAL",
